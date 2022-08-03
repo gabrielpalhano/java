b3fb7d1f2e06c985e64952452603b97096117fc1
gabriel palhano <gabrielpalhano.dev@gmail.com>
2022-08-03 11:10:31 -0300

    Ainda no cap 03 (03/08/2022)
    For e algumas outras coisas!

--- Treino/Treino.java
+++ Treino/Treino.java
@@ -1,13 +1,24 @@
 class Treino {
 	public static void main(String[] args) {
-	double n1 = 6.7;
-	int n2 = (int) n1;
-	
-	System.out.println(n2);
-	
+		int a = 6;
+		int g = a++;
+			System.out.println(g);
+		int y = 6;	
+		int x = ++y;
+			System.out.println(x);
+				System.out.println(a);
 	}
+
 }
 
-/* O objetivo é que todo dia eu treine aqui, qualquer coisa que eu precisar treinar;
+/* O objetivo é que todo dia eu treine aqui qualquer coisa que eu precisar treinar;
 Normalmente, referente aos estudos do dia anterior, a fim de fixar melhor o conteúdo;
 Então é provável que todo dia o código deste arquivo mude!! */
+
+/* Entendo um pouco melhor o método de incremento (pré e pós), ++ na frente da variável a faz receber
+o valor na hora, já o ++ após a variável atribui este valor apenas na próxima linha, ou seja, neste 
+exemplo que usamos, a variável g receberá o valor 6 da variável a e a variável a receberá o incremento
+e se tornará 7, mas a variável g só recebe o valor atual e corta laços com a variável a antes que possa 
+receber o incremento, já no segundo caso, a variável x recebe o valor e o incremento ao mesmo tempo, o 
+que a torna 7 logo de cara! */
+
---
+++ cap-03/For/For.java
@@ -0,0 +1,7 @@
+class For {
+	public static void main (String[] args) {
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Olá 10 vezes");
+		}
+	}
+}
