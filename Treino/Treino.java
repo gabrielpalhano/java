8728ac3c4db2b2d0fe71cb66356d7078cd4caec2
gabriel palhano <gabrielpalhano.dev@gmail.com>
2022-08-14 14:03:34 -0300

    att 01; 14/08/2022

--- Treino/Treino.java
+++ Treino/Treino.java
@@ -1,5 +1,11 @@
 class Treino {
-	
+	public static void main(String[] args) {
+	int i;	
+		//System.out.println("Aqui estão os algarismos compreendidos entre 150 e 300!!");
+			for (i = 150; i <= 300; i++) {
+				//System.out.println(i);
+			} System.out.println(i);
+	}
 }
 
 /* O objetivo é que todo dia eu treine aqui qualquer coisa que eu precisar treinar;
