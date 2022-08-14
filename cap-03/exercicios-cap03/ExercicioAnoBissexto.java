09c7be184a16ed171e7cd189459fed76497054fc
gabriel palhano <gabrielpalhano.dev@gmail.com>
2022-08-14 18:33:04 -0300

    att 05; 14/08/2022
    Exercício proposto na aula 21 do curso "programar em java" do canal "De aluno para aluno"!

--- cap-03/exercicios-cap03/ExercicioAnoBissexto.java
+++ cap-03/exercicios-cap03/ExercicioAnoBissexto.java
@@ -1,24 +1,15 @@
-class Treino {
+class ExercicioAnoBissexto {
 	public static void main(String[] args) {
-		int ab = 1000;
+		int ab = 1004;
 		
 			if ((ab % 4 == 0 && ab % 100 != 0) || (ab % 400 == 0)) {
 				System.out.println("O ano de: "+ab+" é bissexto!!");
 			} else {
 				System.out.println("O ano de: "+ab+" não é bissexto!!");
 			}
-	}
-}
-
-/* O objetivo é que todo dia eu treine aqui qualquer coisa que eu precisar treinar;
-Normalmente, referente aos estudos do dia anterior, a fim de fixar melhor o conteúdo;
-Então é provável que todo dia o código deste arquivo mude!! */
-
-
-/* System.out.println("O ano de: "+ab+" é bissexto!!");
-System.out.println("O ano de: "+ab+" não é bissexto!!"); */
-
-/*	if (ab % 4 != 0) {
+			
+			
+		/*	if (ab % 4 != 0) {
 				System.out.println("O ano de: "+ab+" não é bissexto!!");
 			} else if (ab % 100 != 0) {
 				System.out.println("O ano de: "+ab+" é bissexto!!");
@@ -27,3 +18,9 @@ System.out.println("O ano de: "+ab+" não é bissexto!!"); */
 			} else {
 				System.out.println("O ano de: "+ab+" não é bissexto!!");
 			} */
+	}
+}
+
+
+/* System.out.println("O ano de: "+ab+" é bissexto!!");
+System.out.println("O ano de: "+ab+" não é bissexto!!"); */
