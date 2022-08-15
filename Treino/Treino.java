eb38870a0d25610e575cc59d0e4ec722a5b1366c
gabriel palhano <gabrielpalhano.dev@gmail.com>
2022-08-15 19:31:36 -0300

    att 03; 15/08/2022
    Do While!

--- Treino/Treino.java
+++ Treino/Treino.java
@@ -1,11 +1,17 @@
 class Treino {
 	public static void main(String[] args) {
-		int ab = 1000;
-		
-			if ((ab % 4 == 0 && ab % 100 != 0) || (ab % 400 == 0)) {
-				System.out.println("O ano de: "+ab+" é bissexto!!");
-			} else {
-				System.out.println("O ano de: "+ab+" não é bissexto!!");
+		int g = 1;
+		int p = 1;
+			
+			do{
+				System.out.println(g);
+				++g;
+			} while (g < 1);
+			
+			while (p < 1) {
+				
+				System.out.println(p);
+				++p;
 			}
 	}
 }
@@ -15,15 +21,5 @@ Normalmente, referente aos estudos do dia anterior, a fim de fixar melhor o cont
 Então é provável que todo dia o código deste arquivo mude!! */
 
 
-/* System.out.println("O ano de: "+ab+" é bissexto!!");
-System.out.println("O ano de: "+ab+" não é bissexto!!"); */
 
-/*	if (ab % 4 != 0) {
-				System.out.println("O ano de: "+ab+" não é bissexto!!");
-			} else if (ab % 100 != 0) {
-				System.out.println("O ano de: "+ab+" é bissexto!!");
-			} else if (ab % 400 == 0) {
-				System.out.println("O ano de: "+ab+" é bissexto!!");
-			} else {
-				System.out.println("O ano de: "+ab+" não é bissexto!!");
-			} */
+
---
+++ cap-03/DoWhile/DoWhile.java
@@ -0,0 +1,16 @@
+class DoWhile {
+	public static void main(String[] args) {
+		int g = 1;
+		int p = 1;
+			
+			do{
+				System.out.println(g);
+				g++;
+			} while (g < 1);
+			
+			while (p < 1) {	
+				System.out.println(p);
+				p++;
+			}
+	}
+}
