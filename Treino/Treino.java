6476545f6036665672b86c1ea84a94c2cbb67cd7
gabriel palhano <gabrielpalhano.dev@gmail.com>
2022-08-17 16:09:04 -0300

    att 03; 17/08/2022

--- Treino/Treino.java
+++ Treino/Treino.java
@@ -1,20 +1,13 @@
 class Treino {
 	public static void main(String[] args) {
-		int g = 1;
-		int p = 1;
-			
-			do{
-				System.out.println(g);
-				++g;
-			} while (g < 1);
-			
-			while (p < 1) {
-				
-				System.out.println(p);
-				++p;
-			}
+		int aa = 1200;
+		
+			if((aa % 4 == 0 && aa % 100 != 0) || (aa % 400 == 0)) {
+				System.out.println("O ano de: "+aa+" é bissexto!");
+			} else System.out.println("O ano de: "+aa+" não é bissexto!");
 	}
 }
+
 
 /* O objetivo é que todo dia eu treine aqui qualquer coisa que eu precisar treinar;
 Normalmente, referente aos estudos do dia anterior, a fim de fixar melhor o conteúdo;
