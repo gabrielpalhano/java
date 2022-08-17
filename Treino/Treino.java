f7fdb1058b96c741b8aa6251cda8f66950731af5
gabriel palhano <gabrielpalhano.dev@gmail.com>
2022-08-17 16:36:20 -0300

    att 04; 17/08/2022

--- Treino/Treino.java
+++ Treino/Treino.java
@@ -1,10 +1,8 @@
 class Treino {
 	public static void main(String[] args) {
-		int aa = 1200;
-		
-			if((aa % 4 == 0 && aa % 100 != 0) || (aa % 400 == 0)) {
-				System.out.println("O ano de: "+aa+" é bissexto!");
-			} else System.out.println("O ano de: "+aa+" não é bissexto!");
+		for(int s = 0, c = 1; c <= 10; s++, c++) {
+			System.out.println(c);
+		}
 	}
 }
 
