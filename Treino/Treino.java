47f66889783c5db4216f0de79dae39ebdfacf57c
gabriel palhano <gabrielpalhano.dev@gmail.com>
2022-08-19 16:35:06 -0300

    att 01; 19/08/2022

--- Treino/Treino.java
+++ Treino/Treino.java
@@ -1,25 +1,22 @@
 class Treino {
 	public static void main(String[] args) {
-		long n = 13;
-			do {				
-				if (n % 2 == 0) {
-					n /= 2;
-						System.out.println(n);
-				} else {
-					n = 3 * n + 1;
-						System.out.println(n);
-				} 
-			} while (n != 1);
+		int x;
+		int y;
+		int z;
+			for(x = 0, y = 1; x <= 50; x += y) {
+				
+				z = x + y;
+				y = z;
+						System.out.println(z); 
+					
+				}
 	}
 }
+
 
 /* O objetivo é que todo dia eu treine aqui qualquer coisa que eu precisar treinar;
 Normalmente, referente aos estudos do dia anterior, a fim de fixar melhor o conteúdo;
 Então é provável que todo dia o código deste arquivo mude!! */
 
-/* if(n == 1) {
-				System.out.println("Seu número alcançou o resultado: "+n);
-			}
-				else */
 
 
